7480719600c8556e5a88f5e6cee9ffe5570d6503
harsha <harsha_k_r@yahoo.co.in>
2023-11-15 15:56:28 +0530

    First

---
+++ src/main/java/com/example/demo/HelloApplication1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloApplication1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloApplication1Application.class, args);
+	}
+
+}
